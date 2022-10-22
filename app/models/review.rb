2b43f46cc452adfe67b15511f5f725bcf903da7b
Zsuzsanna Orehoczki <ore.zsuzsanna@gmail.com>
2022-10-22 10:08:37 +0100

    add validations

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+    belongs_to :restaurant
+
+    validates :content, :rating, presence: true
+
+    validates :rating, inclusion: { in: 0..5 }
+
+    validates :rating, numericality: { only_integer: true }
+end
